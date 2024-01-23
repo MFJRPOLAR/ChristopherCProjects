b40844b94b596947fe842ef5c1eeb9b2ec27e417
Christopher Velasco <velascochristopher96@gmail.com>
2024-01-23 10:50:40 -0500

    second commit

---
+++ headers/BasicIo.h
@@ -0,0 +1,17 @@
+// pre-process directives
+
+// create macro values (constants)
+#define FIRST_NAME "Christopher"
+#define LAST_NAME "Velasco"
+#define WEIGHT 220.5 
+#define AGE 19
+
+// include C header file 
+#include <stdio.h>
+
+// function declarations 
+void output(void);
+void input(void);
+
+
+
---
+++ headers/Decisions.h
@@ -0,0 +1,16 @@
+// pre-proessor directives
+
+// crete macro values (constants)
+#define NUM 34568.8
+
+// include C header files 
+#include <stdio.h>
+#include <stdbool.h> 
+
+// function declarations 
+void descisions1(void);
+void descisions2(void);
+void descisions3(void);
+void descisions4(void);
+void descisions5(void);
+void descisions6(void);
